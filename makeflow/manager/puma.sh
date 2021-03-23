29f6398445aa0fd5a804a02626e5bbe984004381
Reetu Tuteja <reetututeja@junonia.hpc.arizona.edu>
2021-03-23 14:46:24 -0700

    add makeflow pipleine

--- makeflow/manager/puma.sh
+++ makeflow/manager/puma.sh
@@ -1,4 +1,3 @@
 #!/bin/bash -l 
 
-srun --nodes=1 --mem=2GB --ntasks=1 --cpus-per-task=4 --time=02:00:00 --job-name=run_hamr --account=lyons-lab --partition=standard --mpi=pmi2 --pty bash -i
-
+srun --nodes=1 --mem=2GB --ntasks=1 --cpus-per-task=4 --time=04:00:00 --job-name=hamrmanager --account=windfall --partition=windfall --mpi=pmi2 --pty bash -i
