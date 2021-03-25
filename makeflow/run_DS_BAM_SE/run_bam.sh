181981b094e1e4f8abd63c8c5c8fe8b9e9f3f408
Reetu Tuteja <reetututeja@r1u04n1.puma.hpc.arizona.edu>
2021-03-25 13:53:38 -0700

    add jx for datastore BAM

---
+++ makeflow/run_DS_BAM_SE/run_bam.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+${HOME}/cctools-7.1.12-x86_64-centos7/bin/makeflow -T wq --jx-args SraAccList.json --jx hamr_bam.jx -a -r 10 -N hamrpipeline -M hamrpipeline -p 60219 -dall -o dall.log --disable-cache $@
