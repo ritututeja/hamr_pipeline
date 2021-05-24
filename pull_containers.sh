2d11741b841600f93ddacb4adb21a12978ef1377
reetututeja <tuteja.reetu@gmail.com>
2021-05-24 12:47:50 -0700

    Update pull_containers.sh

--- pull_containers.sh
+++ pull_containers.sh
@@ -2,3 +2,4 @@ singularity pull docker://reetututeja/hamr_xi:1.4
 singularity pull docker://broadinstitute/gatk3:3.5-0
 singularity pull docker://quay.io/biocontainers/samtools:1.10--h2e538c0_3
 singularity pull docker://quay.io/biocontainers/picard:2.23.8--0
+singularity pull docker://quay.io/biocontainers/hisat2:2.2.0--py27h877ad6c_4
